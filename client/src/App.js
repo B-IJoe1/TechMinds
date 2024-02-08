92f50bd642da45e1ca34105ce060d2177be13a85
B-IJoe1 <72030864+B-IJoe1@users.noreply.github.com>
2024-02-07 19:18:59 -0500

    Revert "set up frontend structure"

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./pages/Main/Main";
-import Navbar from "./components/Navbar/Navbar";
-// import { useApi } from './hooks/use-api';
+import './App.css';
+
+import { useApi } from './hooks/use-api';
 
 function App() {
-  // const { response } = useApi();
+  const { response } = useApi();
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route path="/" element={<Main />}></Route>
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <header className="App-header">
+        <p>
+          {response}
+        </p>
+      </header>
     </div>
   );
 }
