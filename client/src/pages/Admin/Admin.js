aa929626e04078317932c32c9f1d63d56787f447
kpin2 <k_pinto2@massbay.edu>
2024-02-11 19:30:20 -0500

    dropdown cleanup

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,29 +1,32 @@
 import React from "react";
 
 const Admin = () => {
-    return (
-        <div>
-            <h1>Admin</h1>
-            <p>This page is used to add, delete, or update exam records. You can also find a specific exam based on a number of attributes.</p>
+    return (<div>
+        <h1>Admin</h1>
+        <p>This page is used to add, delete, or update exam records. You can also find a specific exam based on a
+            number of attributes.</p>
 
-            <div className="buttons">
-                <button>Add New Exam</button>
-                <br/>
-                <button>Update Exam</button>
-                <button>Delete Exam</button>
-                <br/><br/>Find Exam by: <select>
+        <div className="buttons">
+            <button>Add New Exam</button>
+            <br/>
+            <button>Update Exam</button>
+            <button>Delete Exam</button>
+            <br/><br/>
+            <label>Find Exam by:  &nbsp;
+                <select>
                     <option>Patient ID</option>
                     <option>Exam ID</option>
                     <option>Patient Age</option>
                     <option>Patient Sex</option>
                     <option>Zip Code</option>
-            </select>
-                <input/><button>Find Exam</button>
-
-            </div>
+                </select>
+            </label>
+            <input/>
+            <button>Search</button>
 
         </div>
-);
+
+    </div>);
 }
 
 export default Admin;
